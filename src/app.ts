fd66c5205fcda9a3144d02755bd517f4547db9f2
Andrew Domingues <drewdomi@proton.me>
2024-05-10 14:00:36 -0300

    chore: add prettier

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,34 @@
-import http from "node:http";
-import express from "express";
-import cors from "cors";
-import { Request, Response } from "express";
+import http from 'node:http'
+import express from 'express'
+import cors from 'cors'
+import { Request, Response } from 'express'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(
   express.urlencoded({
     extended: true,
   }),
-);
+)
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   }),
-);
+)
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("API Running at port 3000");
-});
+app.get('/', (_req: Request, res: Response) => {
+  res.send('API Running at port 3000')
+})
 
-const server = http.createServer(app);
-server.on("error", (error) => {
-  console.log(error);
-});
+const server = http.createServer(app)
+server.on('error', error => {
+  console.log(error)
+})
 
 server.listen(port, () => {
-  console.log("API running on port " + port);
-});
+  console.log('API running on port ' + port)
+})
