4b88eba1aad574054d7f632c6a2f70002eaf5e4c
Andrew Domingues <drewdomi@proton.me>
2024-05-10 14:30:21 -0300

    chore: add husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
   {
     languageOptions: { globals: globals.browser },
   },
   ...tseslint.configs.recommended,
-];
+]
